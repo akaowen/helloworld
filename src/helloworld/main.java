439e9e7b2fc864c702510014e710b19db06e5a5b
akaowen <reid.tull@icloud.com>
2015-03-08 16:11:20 -0400

    first commit
    it says hello world

---
+++ src/helloworld/main.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		for(int i = 0; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
