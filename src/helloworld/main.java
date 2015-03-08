cc6843f1c60492c6f37d327a8620c847f6337163
Zevrix Kojote <zevrixkojote@gmail.com>
2015-03-08 16:23:14 -0400

    Changed code around
    Removed for loop

--- src/helloworld/main.java
+++ src/helloworld/main.java
@@ -1,14 +1,9 @@
 package helloworld;
 
-public class main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class main 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World");
-		for(int i = 0; i <= 10; i++)
-		{
-			System.out.println(i);
-		}
 	}
-
 }
